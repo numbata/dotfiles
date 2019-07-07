e011ba492c119856406698e5da29ffd5d011f678
Andrey Subbota <subbota@gmail.com>
2019-07-07 16:19:34 +0300

    Become and dotfiles fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,7 +34,7 @@
 
 echo "Play ansible playbook"
 if [ -z $1 ]; then
-  ansible-playbook -i "localhost," -D -c local playbook.yml --ask-become-pass -vvvv
+  ansible-playbook -K -i "localhost," -D -c local playbook.yml -vvvv
 else
-  ansible-playbook -i "localhost," -D -c local playbook.yml --ask-become-pass -vvvv --tags $1
+  ansible-playbook -K -i "localhost," -D -c local playbook.yml -vvvv --tags $1
 fi
