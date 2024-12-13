fd118f2ecea655082945a33e175fa56b4a40630e
Andrey Subbota <subbota@gmail.com>
2024-12-13 02:53:29 +0100

    Init

---
+++ dot_wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+
+local config = {
+    automatically_reload_config = true,
+    font = wezterm.font 'Fira Mono',
+    font_size = 10.0,
+    -- color_scheme = 'Windows 10 Light (base16)',
+    color_scheme = 'Londontube (light) (terminal.sexy)',
+    --color_scheme = 'Lunaria Light (Gogh)',
+    enable_tab_bar = false,
+    window_decorations = "RESIZE",
+    native_macos_fullscreen_mode = true,
+    hide_tab_bar_if_only_one_tab = true,
+    front_end = "Software",
+    keys = {
+        { key = 'LeftArrow', mods = 'CMD|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
+        { key = 'RightArrow', mods = 'CMD|SHIFT', action = wezterm.action.ActivateTabRelative(1) },
+    },
+}
+
+return config
+
