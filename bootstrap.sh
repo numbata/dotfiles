ac2dd26f2bb7097469446117ebc5709843acc67d
Andrey Subbota <subbota@gmail.com>
2024-12-15 08:50:08 +0100

    Fix key populating

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,32 @@
 
 set -e # -e: exit on error
 
+export HOMEBREW_NO_INTERACTIVE=1
+export HOMEBREW_NO_ENV_HINTS=1
+export HOMEBREW_NO_ANALYTICS=1
+
+# Install Homebrew if not present
+if ! command -v brew &>/dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.zshrc
+    eval "$(/usr/local/bin/brew shellenv)"
+else
+    echo "Homebrew already installed."
+fi
+
+# Check if 1Password CLI is installed
+if ! command -v op &>/dev/null; then
+  echo "Installing 1Password CLI..."
+  brew install --quiet 1password/tap/1password-cli
+fi
+
+# Authenticate to 1Password if not already signed in
+if ! op whoami &>/dev/null; then
+  echo "Signing in to 1Password CLI..."
+  eval $(op account add --signin --address my.1password.com)
+fi
+
 declare -r DOTFILES_REPO_URL="https://github.com/numbata/dotfiles"
 
 if [ ! "$(command -v chezmoi)" ]; then
