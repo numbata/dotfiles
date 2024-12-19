1aaa6f27043ac4e129bfd8bd65d4fc712de4e4c7
Andrey Subbota <subbota@gmail.com>
2024-12-19 10:34:18 +0100

    Back to NERDTree

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -12,6 +12,12 @@ local config = {
     native_macos_fullscreen_mode = true,
     hide_tab_bar_if_only_one_tab = true,
     front_end = "Software",
+    audible_bell = "Disabled"
+    config.visual_bell = {
+        fade_in_duration_ms = 75,
+        fade_out_duration_ms = 75,
+        target = 'CursorColor',
+    },
     keys = {
         { key = 'LeftArrow', mods = 'CMD|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
         { key = 'RightArrow', mods = 'CMD|SHIFT', action = wezterm.action.ActivateTabRelative(1) },
